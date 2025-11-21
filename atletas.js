186c19c4bc8f0256872eb531b601d3b032979f99
Paulo Vieira <pvieiradevweb@gmail.com>
2025-11-21 16:59:18 -0300

    Create atletas.js

---
+++ atletas.js
@@ -0,0 +1,38 @@
+let atletas = [
+ {
+   nome: "Cesar Abascal",
+   notas: [10, 9.34, 8.42, 10, 7.88]
+ },
+ {
+   nome: "Fernando Puntel",
+   notas:  [8, 10, 10, 7, 9.33]
+ },
+ {
+   nome: "Daiane Jelinsky",
+   notas: [7, 10, 9.5, 9.5, 8]
+ },
+ {
+   nome: "Bruno Castro",
+   notas: [10, 10, 10, 9, 9.5]
+ }
+];
+
+function calcularMedias(atletas) {
+  for (let i = 0; i < atletas.length; i++) {
+
+    let notas = atletas[i].notas.slice().sort((a, b) => a - b);
+
+    let notasComputadas = notas.slice(1, 4);
+
+    let soma = 0;
+    notasComputadas.forEach(n => soma += n);
+
+    let media = soma / notasComputadas.length;
+
+    console.log(`Atleta: ${atletas[i].nome}`);
+    console.log(`Notas Obtidas: ${atletas[i].notas.join(",")}`);
+    console.log(`Média Válida: ${media}\n`);
+  }
+}
+
+calcularMedias(atletas);
